29cc610a253f851e6e616d22a6bb157b984abc17
Sameer Rahmani <lxsameer@gnu.org>
2018-06-02 19:25:33 +0100

    Initial commit

---
+++ test/manifold_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns manifold-test.core-test
+  (:require [clojure.test :refer :all]
+            [manifold-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
