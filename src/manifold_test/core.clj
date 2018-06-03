cce98e55a619d529762728c6fa891610901dde86
Sameer Rahmani <lxsameer@gnu.org>
2018-06-04 00:46:26 +0100

    better thread pool watch has been added

--- src/manifold_test/core.clj
+++ src/manifold_test/core.clj
@@ -6,12 +6,13 @@
 
 (defn normal-execution-single-stream
   [s1]
-  (s/consume #(println "<<<<<<<<<< " %) s1)
+  (s/consume (fn [x]
+               (Thread/sleep 100)
+               (println (.getName (Thread/currentThread)) "  " x)) s1)
   (loop [x 0]
     (when (< x 200)
       (s/put! s1 x)
       (recur (inc x)))))
-
 
 
 (defn setup-exit
@@ -23,10 +24,14 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [s1           (s/stream* {:buffer-size 100
-                                 :executor (e/fixed-thread-executor 20)})]
+  (let [dummy (s/stream)
+        s1 (s/stream* {:buffer-size 100
+                       :executor (e/fixed-thread-executor 20)})]
 
-    (setup-exit #(s/close! s1))
+    (setup-exit (fn []
+                  (println "xxxxxxxxxxxxxxxxxxxxxxx")
+                  (s/close! s1)))
+
     (println "Manifold test->")
     (normal-execution-single-stream s1)
-    (Thread/sleep 5000)))
+    @(s/take! dummy)))
